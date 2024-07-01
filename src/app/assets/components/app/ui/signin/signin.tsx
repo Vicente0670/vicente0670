29010aa87f8cea53bde36fb941c533d21687de81
Vicente0670 <122196085+Vicente0670@users.noreply.github.com>
2024-07-01 17:48:12 -0500

    adding

--- src/app/assets/components/app/ui/signin/signin.tsx
+++ src/app/assets/components/app/ui/signin/signin.tsx
@@ -2,13 +2,11 @@ import {auth} from "@/auth";
 import Image from "next/image";
 export default async function SignInProfile() {
   const session = await auth();
-
   if (!session?.user) return (
     <>
       <a className="access" href="/access">Sign In</a>
     </>
   )
-
   if (session.user.image) fetch(session.user.image as string);
 
   return (
